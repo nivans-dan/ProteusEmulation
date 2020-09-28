aa5b624555e646f1f4bbcda9fc61c590949c3545
Daniel Nava Martínez <70957627+lNivans@users.noreply.github.com>
2020-09-27 20:53:22 -0500

    Add files via upload

---
+++ Prac0.ino
@@ -0,0 +1,29 @@
+void setup() 
+{
+    pinMode(13, OUTPUT);
+    pinMode(12, OUTPUT);
+    pinMode(11, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(8, OUTPUT);
+}
+
+void loop() 
+{ 
+    digitalWrite(13, HIGH);
+    digitalWrite(8, HIGH);
+    delay(6000); 
+    digitalWrite(12,HIGH);
+    delay(6000);
+    digitalWrite(13, LOW);
+    digitalWrite(8, LOW);
+    digitalWrite(12,LOW);
+    digitalWrite(11, HIGH);
+    digitalWrite(10, HIGH);
+    delay(6000);
+    digitalWrite(9,HIGH);
+    delay(6000);
+    digitalWrite(11, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(9,LOW); 
+}
